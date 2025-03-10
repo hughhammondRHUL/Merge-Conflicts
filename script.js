4e115bdb2e6d82ea0e4b0fac2356cab390f1f454
hughhammondRHUL <hugh.hammond@rhul.ac.uk>
2025-03-10 10:03:41 +0000

    added new param to console log

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ param1 = true;
 param2 = false;
 param3 = param1;
 param4 = param2;
-console.log("param1 and param 2 are ", param1, param2)
+console.log("param1 and param 2 are ", param1, param2, param3)
